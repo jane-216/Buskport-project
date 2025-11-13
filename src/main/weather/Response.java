83cc57b7cca2b4d9b46545fbe51f57d328fc0031
jane-216 <janeoh0802@gmail.com>
2025-11-13 15:14:33 +0900

    Create Response.java

---
+++ src/main/weather/Response.java
@@ -0,0 +1,54 @@
+package com.buskport.weather.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class KmaUltraSrtNcstResponse {
+
+    private Response response;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Response {
+        private Header header;
+        private Body body;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Header {
+        private String resultCode;
+        private String resultMsg;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Body {
+        private String dataType;
+        private Items items;
+        private int pageNo;
+        private int numOfRows;
+        private int totalCount;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Items {
+        private List<Item> item;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Item {
+        private String baseDate;
+        private String baseTime;
+        private String category;
+        private int nx;
+        private int ny;
+        private String obsrValue;
+    }
+}

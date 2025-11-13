2cc6bf94a998e514f7568f8d022ca5196fc9eccc
jane-216 <janeoh0802@gmail.com>
2025-11-13 15:15:43 +0900

    Create WeatherInvoker.java

---
+++ src/main/WeatherInvoker.java
@@ -0,0 +1,51 @@
+package com.buskport.weather.invoker;
+
+import com.buskport.weather.model.KmaUltraSrtNcstResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@RequiredArgsConstructor
+public class KmaWeatherInvoker {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${kma.ultra-srt-ncst-url}")
+    private String ultraSrtNcstUrl;
+
+    @Value("${kma.auth-key}")
+    private String authKey;
+
+    /**
+     * @param baseDate yyyyMMdd (ex: 20251113)
+     * @param baseTime HHmm   (ex: 0700)
+     * @param nx       
+     * @param ny       
+     */
+    public KmaUltraSrtNcstResponse getUltraShortNowcast(String baseDate,
+                                                        String baseTime,
+                                                        int nx,
+                                                        int ny) {
+
+        String uri = UriComponentsBuilder.fromHttpUrl(ultraSrtNcstUrl)
+                .queryParam("pageNo", 1)
+                .queryParam("numOfRows", 1000)
+                .queryParam("dataType", "JSON")
+                .queryParam("base_date", baseDate)
+                .queryParam("base_time", baseTime)
+                .queryParam("nx", nx)
+                .queryParam("ny", ny)
+                .queryParam("authKey", authKey)
+                .toUriString();
+
+        ResponseEntity<KmaUltraSrtNcstResponse> responseEntity =
+                restTemplate.exchange(uri, HttpMethod.GET, null, KmaUltraSrtNcstResponse.class);
+
+        return responseEntity.getBody();
+    }
+}
